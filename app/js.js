7d1bf9f7b9a41392cbafa763eadc32b1795995f5
XYZmikkel-tech <mikkelogbobermegasej@gmail.com>
2026-03-06 09:07:03 +0100

    Add files via upload

---
+++ app/js.js
@@ -0,0 +1,4 @@
+document.getElementById('startBtn').addEventListener('click', function() {
+    const testLink = 'https://www.youtube.com/watch?v=uf8F97ONQbc';
+    window.location.href = testLink;
+});
